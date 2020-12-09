be75b4275ec5eeae2eea2abe9dee2d5e718f3ba6
pieeri <eric@rubicubix.co.uk>
2020-12-09 19:44:00 +0000

    Return run URL once a plan has been triggered

--- src/terraform_client/api.py
+++ src/terraform_client/api.py
@@ -1,9 +1,4 @@
-import logging
-
 from . import client
-
-
-logger = logging.getLogger(__name__)
 
 
 RUN_URL_TEMPLATE = "https://app.terraform.io/app/{organization}/workspaces/{workspace}/runs/{id}"
@@ -25,10 +20,9 @@ def update_workspace_variable(
 
     # Update variable with the new value
     tf_client.update_variable(tf_vars[variable], value)
-    logger.info(f"Updated Terraform {variable!r} value to {value!r}")
 
 
-def create_workspace_run(organization: str, workspace: str, token: str, message: str) -> None:
+def create_workspace_run(organization: str, workspace: str, token: str, message: str) -> str:
     # Authenticate client
     tf_client = client.TerraformClient()
     tf_client.set_auth_header(token)
@@ -38,5 +32,4 @@ def create_workspace_run(organization: str, workspace: str, token: str, message:
 
     # Create and apply a new plan
     run_id = tf_client.create_run(workspace_id, message)
-    run_url = RUN_URL_TEMPLATE.format(organization=organization, workspace=workspace, id=run_id)
-    logger.info(f"Running Terraform plan for {workspace}: {run_url}")
+    return RUN_URL_TEMPLATE.format(organization=organization, workspace=workspace, id=run_id)
