663485bc3c7c963e215d98adce62beeb7b782300
pieeri <eric@rubicubix.co.uk>
2020-12-12 14:42:46 +0000

    Factorize noostf CLI unit tests

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -9,27 +9,23 @@ def ctx():
     return context.Context()
 
 
+@pytest.fixture(
+    params=[
+        (None, "test_token"),
+        ("test_organisation", None),
+    ]
+)
+def secrets(request):
+    return request.param
+
+
 class TestUpdate:
-    @pytest.mark.parametrize(
-        "organisation,token",
-        [
-            (None, "test_token"),
-            ("test_organisation", None),
-        ],
-    )
-    def test_invalid_credentials_raise_error(self, ctx, organisation, token):
+    def test_invalid_credentials_raise_error(self, ctx, secrets):
         with pytest.raises(AssertionError):
-            cli.update(ctx, organisation=organisation, token=token)
+            cli.update(ctx, organisation=secrets[0], token=secrets[1])
 
 
 class TestRun:
-    @pytest.mark.parametrize(
-        "organisation,token",
-        [
-            (None, "test_token"),
-            ("test_organisation", None),
-        ],
-    )
-    def test_invalid_credentials_raise_error(self, ctx, organisation, token):
+    def test_invalid_credentials_raise_error(self, ctx, secrets):
         with pytest.raises(AssertionError):
-            cli.run(ctx, organisation=organisation, token=token)
+            cli.run(ctx, organisation=secrets[0], token=secrets[1])
